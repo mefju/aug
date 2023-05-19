1952a9c62c8f1b1d79b570af335a8bf00dea51c2
Maciej Ostaszewski <maciej@ostaszew.ski>
2023-05-19 11:03:24 +0200

    feat: initial stack machine

---
+++ stack_machine.py
@@ -0,0 +1,11 @@
+import sys
+from antlr4 import *
+from sasmLexer import sasmLexer
+from sasmParser import sasmParser
+
+def main(argv):
+    print("dziala")
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
