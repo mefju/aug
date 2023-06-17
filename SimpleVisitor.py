967fe1db5fb6b2c4b42b0c576c1b13792b52f809
Maciej Ostaszewski <maciej@ostaszew.ski>
2023-06-17 22:12:32 +0200

    last fixes

--- SimpleVisitor.py
+++ SimpleVisitor.py
@@ -1,6 +1,6 @@
-# Generated from Simple.g4 by ANTLR 4.12.0
+# Generated from Simple.g4 by ANTLR 4.13.0
 from antlr4 import *
-if __name__ is not None and "." in __name__:
+if "." in __name__:
     from .SimpleParser import SimpleParser
 else:
     from SimpleParser import SimpleParser
